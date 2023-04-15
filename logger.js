e422440f67c356577ef6b22e604ff2f99937d24f
Kayode-Olumo <46356143+Kayode-Olumo@users.noreply.github.com>
2023-04-15 12:29:00 +0100

    first commit

---
+++ logger.js
@@ -0,0 +1,9 @@
+const url ='https://mylogger.io/log';
+
+const log = (message) => {
+    // Send http request
+    console.log(message);
+}
+
+module.exports = log;
+// module.export.endpoint = url;
